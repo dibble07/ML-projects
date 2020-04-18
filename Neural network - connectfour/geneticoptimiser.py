a95ca67ab0cee7ec50580d7274792585f425a85b
dibble07 <63882038+dibble07@users.noreply.github.com>
2020-04-18 12:20:12 +0100

    Tried a 2 stage GA process but it's still pretty poor

--- Neural network - connectfour/geneticoptimiser.py
+++ Neural network - connectfour/geneticoptimiser.py
@@ -3,8 +3,7 @@ import random
 random.seed(42)
 
 # Define function
-def floatopt(toolbox, tools, pop_sz, MUTPB, g_max):
-    # create/register required DEAP attributes
+def floatopt(toolbox, tools, pop_sz, MUTPB, g_max, fit_max):
     # initialise
     g = 0
     pop = toolbox.population(n=pop_sz)
@@ -15,14 +14,12 @@ def floatopt(toolbox, tools, pop_sz, MUTPB, g_max):
     # print the stats
     fits = [ind.fitness.values[0] for ind in pop]
     print(f"Evolution {g} : Min {min(fits):.3f}, Max {max(fits):.3f}, Mean {sum(fits) / len(pop):.3f}")
-    best_ind = [tools.selBest(pop, len(pop))]
-    while g < g_max:
+    best_ind = [tools.selBest(pop, 1)[0]]
+    while g < g_max and max(fits)<fit_max:
         g+=1
-        # print("pop", pop, [ind.fitness.values for ind in pop])
         # select and clone the next generation individuals
         unchanged = toolbox.select(pop, int(len(pop)/4))
         unchanged = list(map(toolbox.clone, unchanged))
-        # print("unchanged", unchanged, [ind.fitness.values for ind in unchanged])
         # apply crossover and mutation
         mated = []
         for i in range(int((len(pop)-len(unchanged))/2)):
@@ -33,11 +30,6 @@ def floatopt(toolbox, tools, pop_sz, MUTPB, g_max):
             mated.append(child1)
             mated.append(child2)
         pop[:] = unchanged + mated
-        # for child1, child2 in zip(offspring[int(len(pop)/2)::2], offspring[int(len(pop)/2)+1::2]):
-        #     if random.random() < CXPB:
-        #         toolbox.mate(child1, child2)
-        #         del child1.fitness.values
-        #         del child2.fitness.values
         for mutant in pop:
             if random.random() < MUTPB:
                 toolbox.mutate(mutant)
@@ -47,9 +39,8 @@ def floatopt(toolbox, tools, pop_sz, MUTPB, g_max):
         fitnesses = map(toolbox.evaluate, invalid_ind)
         for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness.values = fit
-        # print("mutated", pop, [ind.fitness.values for ind in pop])
         # save the best
-        best_ind.append(tools.selBest(pop, len(pop)))
+        best_ind.append(tools.selBest(pop, 1)[0])
         # print the stats
         if g % 1 == 0:
             fits = [ind.fitness.values[0] for ind in pop]
