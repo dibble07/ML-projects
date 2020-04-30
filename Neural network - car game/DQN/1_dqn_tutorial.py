ec6dfbfd93c06f5d6ef08c01e1290d50ec699e89
dibble07 <63882038+dibble07@users.noreply.github.com>
2020-04-30 13:57:09 +0100

    DQN example using tf-agents

---
+++ Neural network - car game/DQN/1_dqn_tutorial.py
@@ -0,0 +1,165 @@
+# Train a Deep Q Network with TF-Agents
+
+import base64
+import imageio
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL.Image
+
+import tensorflow as tf
+tf.compat.v1.enable_v2_behavior()
+
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import q_network
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+
+def create_policy_eval_video(policy, filename, num_episodes=5, fps=30):
+  filename = filename + ".mp4"
+  with imageio.get_writer(filename, fps=fps) as video:
+    for _ in range(num_episodes):
+      time_step = eval_env.reset()
+      video.append_data(eval_py_env.render())
+      while not time_step.is_last():
+        action_step = policy.action(time_step)
+        time_step = eval_env.step(action_step.action)
+        video.append_data(eval_py_env.render())
+
+def compute_avg_return(environment, policy, num_episodes=10):
+
+  total_return = 0.0
+  for _ in range(num_episodes):
+
+    time_step = environment.reset()
+    episode_return = 0.0
+
+    while not time_step.is_last():
+      action_step = policy.action(time_step)
+      time_step = environment.step(action_step.action)
+      episode_return += time_step.reward
+    total_return += episode_return
+
+  avg_return = total_return / num_episodes
+  return avg_return.numpy()[0]
+
+def collect_step(environment, policy, buffer):
+  time_step = environment.current_time_step()
+  action_step = policy.action(time_step)
+  next_time_step = environment.step(action_step.action)
+  traj = trajectory.from_transition(time_step, action_step, next_time_step)
+
+  # Add trajectory to the replay buffer
+  buffer.add_batch(traj)
+
+def collect_data(env, policy, buffer, steps):
+  for _ in range(steps):
+    collect_step(env, policy, buffer)
+
+# Hyperparameters
+num_iterations = 10000
+initial_collect_steps = 1000
+collect_steps_per_iteration = 1
+replay_buffer_max_length = 100000
+batch_size = 64 
+learning_rate = 1e-3
+log_interval = 200 
+num_eval_episodes = 10
+eval_interval = 1000
+
+# Environment
+# initialise
+env_name = 'CartPole-v0'
+env = suite_gym.load(env_name)
+time_step = env.reset()
+PIL.Image.fromarray(env.render())
+# display specs
+# print('Observation Spec:', env.time_step_spec().observation) # the position and velocity of the cart and pole 
+# print('Reward Spec:', env.time_step_spec().reward)
+# print('Action Spec:', env.action_spec()) # 0/1 — move left/right
+# print('Time step:', time_step)
+# print('Next time step:', env.step(np.array(1, dtype=np.int32)))
+# load python environment and convert to tensforflow environment
+# train_py_env = suite_gym.load(env_name)
+# eval_py_env = suite_gym.load(env_name)
+train_env = tf_py_environment.TFPyEnvironment(suite_gym.load(env_name))
+eval_env = tf_py_environment.TFPyEnvironment(suite_gym.load(env_name))
+
+# Agent
+fc_layer_params = (100,)
+q_net = q_network.QNetwork(
+    train_env.observation_spec(),
+    train_env.action_spec(),
+    fc_layer_params=fc_layer_params)
+optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
+train_step_counter = tf.Variable(0)
+agent = dqn_agent.DqnAgent(
+    train_env.time_step_spec(),
+    train_env.action_spec(),
+    q_network=q_net,
+    optimizer=optimizer,
+    td_errors_loss_fn=common.element_wise_squared_loss,
+    train_step_counter=train_step_counter)
+agent.initialize()
+# policies
+eval_policy = agent.policy
+collect_policy = agent.collect_policy
+random_policy = random_tf_policy.RandomTFPolicy(train_env.time_step_spec(),
+                                                train_env.action_spec())
+# metrics and evaluation
+replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+    data_spec=agent.collect_data_spec,
+    batch_size=train_env.batch_size,
+    max_length=replay_buffer_max_length)
+agent.collect_data_spec
+agent.collect_data_spec._fields
+# data collection
+collect_data(train_env, random_policy, replay_buffer, steps=100)
+dataset = replay_buffer.as_dataset(
+    num_parallel_calls=3, 
+    sample_batch_size=batch_size, 
+    num_steps=2).prefetch(3)
+iterator = iter(dataset)
+
+# Training the agent
+agent.train = common.function(agent.train) # optimize by wrapping some of the code in a graph using TF function.
+agent.train_step_counter.assign(0)
+avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+returns = [avg_return]
+for _ in range(num_iterations):
+
+  # Collect a few steps using collect_policy and save to the replay buffer.
+  for _ in range(collect_steps_per_iteration):
+    collect_step(train_env, agent.collect_policy, replay_buffer)
+
+  # Sample a batch of data from the buffer and update the agent's network.
+  experience, unused_info = next(iterator)
+  train_loss = agent.train(experience).loss
+
+  step = agent.train_step_counter.numpy()
+
+  if step % log_interval == 0:
+    print('step = {0}: loss = {1}'.format(step, train_loss))
+
+  if step % eval_interval == 0:
+    avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+    print('step = {0}: Average Return = {1}'.format(step, avg_return))
+    returns.append(avg_return)
+
+# Visualization
+iterations = range(0, num_iterations + 1, eval_interval)
+plt.plot(iterations, returns)
+plt.ylabel('Average Return')
+plt.xlabel('Iterations')
+plt.ylim(top=250)
+create_policy_eval_video(agent.policy, "trained-agent")
+create_policy_eval_video(random_policy, "random-agent")
+plt.show()
