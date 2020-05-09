62a14770dc69802f725c6aaac34749c5b359dcc7
dibble07 <63882038+dibble07@users.noreply.github.com>
2020-05-09 10:29:26 +0100

    changed cargame from random to fixed integer

--- Neural network - car game/DQN/homegym.py
+++ Neural network - car game/DQN/homegym.py
@@ -277,10 +277,8 @@ class CarGameEnv(py_environment.PyEnvironment):
 class CardGameEnv(py_environment.PyEnvironment):
 
   def __init__(self):
-    self._action_spec = array_spec.BoundedArraySpec(
-        shape=(), dtype=np.int32, minimum=0, maximum=1, name='action')
-    self._observation_spec = array_spec.BoundedArraySpec(
-        shape=(1,), dtype=np.int32, minimum=0, name='observation')
+    self._action_spec = array_spec.BoundedArraySpec(shape=(), dtype=np.int32, minimum=0, maximum=1, name='action')
+    self._observation_spec = array_spec.BoundedArraySpec(shape=(1,), dtype=np.int32, minimum=0, name='observation')
     self._state = 0
     self._episode_ended = False
 
@@ -298,22 +296,17 @@ class CardGameEnv(py_environment.PyEnvironment):
   def _step(self, action):
 
     if self._episode_ended:
-      # The last action ended the episode. Ignore the current action and start
-      # a new episode.
       return self.reset()
 
-    # Make sure episodes don't go on forever.
-    if action == 1:
-      self._episode_ended = True
-    elif action == 0:
-      new_card = np.random.randint(1, 11)
+    if action == 0:
+      new_card = 4#np.random.randint(1, 11)
       self._state += new_card
-    else:
-      raise ValueError('`action` should be 0 or 1.')
+    elif action == 1:
+      self._episode_ended = True
 
     if self._episode_ended or self._state >= 21:
       reward = self._state - 21 if self._state <= 21 else -21
       return ts.termination(np.array([self._state], dtype=np.int32), reward)
     else:
-      return ts.transition(
-          np.array([self._state], dtype=np.int32), reward=0.0, discount=1.0)
+      reward = 0
+      return ts.transition(np.array([self._state], dtype=np.int32), reward, discount=1.0)
