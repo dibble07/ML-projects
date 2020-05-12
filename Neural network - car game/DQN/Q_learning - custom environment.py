dacbbca7c8f316f0c1dd9623b6afed5110b65773
dibble07 <63882038+dibble07@users.noreply.github.com>
2020-05-12 08:37:44 +0100

    shadowing original environment well

--- Neural network - car game/DQN/Q_learning - custom environment.py
+++ Neural network - car game/DQN/Q_learning - custom environment.py
@@ -1,76 +1,176 @@
-import cv2
-from homegym import BlobEnv
-import matplotlib.pyplot as plt
-from matplotlib import style
-style.use("ggplot")
 import numpy as np
-import pickle
 from PIL import Image
+import cv2
+import matplotlib.pyplot as plt
+import pickle
+from matplotlib import style
 import time
+from homegym import BlobEnv
 
-# Define variables
-HM_EPISODES = 24_000
+style.use("ggplot")
+
+shadow_env = BlobEnv()
+SIZE = shadow_env.SIZE
+HM_EPISODES = 6_000
+MOVE_PENALTY = 1
+ENEMY_PENALTY = 300
+FOOD_REWARD = 25
 epsilon = 0.9
 EPS_DECAY = 0.9998  # Every episode will be epsilon*EPS_DECAY
-SHOW_EVERY = 3000  # how often to play through env visually.
+SHOW_EVERY = 1_000  # how often to play through env visually.
+
 start_q_table = None # None or Filename
+
 LEARNING_RATE = 0.1
 DISCOUNT = 0.95
 
-# Initialise Q table
+PLAYER_N = 1  # player key in dict
+FOOD_N = 2  # food key in dict
+ENEMY_N = 3  # enemy key in dict
+
+# the dict!
+d = {1: (255, 175, 0), 2: (0, 255, 0), 3: (0, 0, 255)}
+
+class Blob:
+    def __init__(self):
+        self.x = np.random.randint(0, SIZE)
+        self.y = np.random.randint(0, SIZE)
+
+    def __str__(self):
+        return f"{self.x}, {self.y}"
+
+    def __sub__(self, other):
+        return (self.x-other.x, self.y-other.y)
+
+    def action(self, choice):
+        '''
+        Gives us 4 total movement options. (0,1,2,3)
+        '''
+        if choice == 0:
+            self.move(x=1, y=1)
+        elif choice == 1:
+            self.move(x=-1, y=-1)
+        elif choice == 2:
+            self.move(x=-1, y=1)
+        elif choice == 3:
+            self.move(x=1, y=-1)
+
+    def move(self, x=False, y=False):
+
+        # If no value for x, move randomly
+        if not x:
+            self.x += np.random.randint(-1, 2)
+        else:
+            self.x += x
+
+        # If no value for y, move randomly
+        if not y:
+            self.y += np.random.randint(-1, 2)
+        else:
+            self.y += y
+
+
+        # If we are out of bounds, fix!
+        if self.x < 0:
+            self.x = 0
+        elif self.x > SIZE-1:
+            self.x = SIZE-1
+        if self.y < 0:
+            self.y = 0
+        elif self.y > SIZE-1:
+            self.y = SIZE-1
+
 if start_q_table is None:
     q_table = {}
     for i in range(-SIZE+1, SIZE):
         for ii in range(-SIZE+1, SIZE):
             for iii in range(-SIZE+1, SIZE):
                     for iiii in range(-SIZE+1, SIZE):
-                            q_table[(i, ii, iii, iiii)] = [np.random.uniform(-5, 0) for i in range(4)]
+                            q_table[((i, ii, iii, iiii))] = [np.random.uniform(-5, 0) for i in range(4)]
 else:
     with open(start_q_table, "rb") as f:
         q_table = pickle.load(f)
 
-# Train and update Q table
 episode_rewards = []
-environment = BlobEnv()
 for episode in range(HM_EPISODES):
     if episode % SHOW_EVERY == 0 and episode > 0:
-        print(f"#{episode}: epsilon = {epsilon} , {SHOW_EVERY} ep mean = {np.mean(episode_rewards[-SHOW_EVERY:])}")
+        print(f"on #{episode}, epsilon is {epsilon}")
+        print(f"{SHOW_EVERY} ep mean: {np.mean(episode_rewards[-SHOW_EVERY:])}")
         show = True
     else:
         show = False
 
+    new_obs_shadow = shadow_env.reset()
+    player = shadow_env.player
+    food = shadow_env.food
+    enemy = shadow_env.enemy
     episode_reward = 0
-    obs = environment.reset()
-    for __ in range(200):
-        # get action
+    for i in range(200):
+        obs_shadow = new_obs_shadow
+        obs = ((player-food) + (player-enemy))
+        if obs != obs_shadow:
+            print(obs, obs_shadow)
         if np.random.random() > epsilon:
             action = np.argmax(q_table[obs])
         else:
             action = np.random.randint(0, 4)
-        new_obs, reward, done = environment.step(action)
-        episode_reward += reward
-        # update Q table
+        player.action(action)
+        new_obs_shadow, reward_shadow, done_shadow = shadow_env.step(action)
+
+        #### MAYBE ###
+        #enemy.move()
+        #food.move()
+        ##############
+
+        if player.x == enemy.x and player.y == enemy.y:
+            reward = -ENEMY_PENALTY
+        elif player.x == food.x and player.y == food.y:
+            reward = FOOD_REWARD
+        else:
+            reward = -MOVE_PENALTY
+        if reward != reward_shadow:
+            print(reward, reward_shadow)
+        ## NOW WE KNOW THE REWARD, LET'S CALC YO
+        # first we need to obs immediately after the move.
+        new_obs = ((player-food) + (player-enemy))
         max_future_q = np.max(q_table[new_obs])
         current_q = q_table[obs][action]
-        new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+
+        if reward == FOOD_REWARD:
+            new_q = FOOD_REWARD
+        else:
+            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
         q_table[obs][action] = new_q
-        # visualise
+
         if show:
-            environment.render()
-        # break loop if done
-        if done:
+            env = np.zeros((SIZE, SIZE, 3), dtype=np.uint8)  # starts an rbg of our size
+            env[food.x][food.y] = d[FOOD_N]  # sets the food location tile to green color
+            env[player.x][player.y] = d[PLAYER_N]  # sets the player tile to blue
+            env[enemy.x][enemy.y] = d[ENEMY_N]  # sets the enemy location to red
+            img = Image.fromarray(env, 'RGB')  # reading to rgb. Apparently. Even tho color definitions are bgr. ???
+            img = img.resize((300, 300))  # resizing so we can see our agent in all its glory.
+            cv2.imshow("image", np.array(img))  # show it!
+            if reward == FOOD_REWARD or reward == -ENEMY_PENALTY:  # crummy code to hang at the end if we reach abrupt end for good reasons or not.
+                if cv2.waitKey(500) & 0xFF == ord('q'):
+                    break
+            else:
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+        episode_reward += reward
+        if reward == FOOD_REWARD or reward == -ENEMY_PENALTY:
             break
 
+    #print(episode_reward)
     episode_rewards.append(episode_reward)
     epsilon *= EPS_DECAY
 
 moving_avg = np.convolve(episode_rewards, np.ones((SHOW_EVERY,))/SHOW_EVERY, mode='valid')
 
-# Visualise outputs
 plt.plot([i for i in range(len(moving_avg))], moving_avg)
 plt.ylabel(f"Reward {SHOW_EVERY}ma")
 plt.xlabel("episode #")
 plt.show()
 
-# with open(f"qtable-{int(time.time())}.pickle", "wb") as f:
-#     pickle.dump(q_table, f)
+with open(f"qtable-{int(time.time())}.pickle", "wb") as f:
+    pickle.dump(q_table, f)
