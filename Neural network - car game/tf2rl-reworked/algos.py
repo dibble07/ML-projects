fa49351281b24b4ac6157bf8d835841121fe40dc
dibble07 <63882038+dibble07@users.noreply.github.com>
2020-05-30 13:41:13 +0100

    removed saving for now - aligning NN approaches in each policy algo

--- Neural network - car game/tf2rl-reworked/algos.py
+++ Neural network - car game/tf2rl-reworked/algos.py
@@ -1,8 +1,9 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
+from tensorflow.keras import Input, Model
 
-def update_target_variables(target_variables, source_variables, tau=1.0, use_locking=False):#, name="update_target_variables"
+def update_target_variables(target_variables, source_variables, tau=1.0, use_locking=False):
 
     def update_op(target_variable, source_variable, tau):
         return target_variable.assign(tau * source_variable + (1.0 - tau) * target_variable, use_locking)
@@ -22,7 +23,8 @@ def huber_loss(x, delta=1.):
 
 
 class QFunc(tf.keras.Model):
-    def __init__(self, state_shape, action_dim, units=[32, 32], name="QFunc", enable_dueling_dqn=False):
+    def __init__(self, state_shape, action_dim, units=[16, 8], name="QFunc", enable_dueling_dqn=False, load_model=None):
+
         super().__init__(name=name)
         self._enable_dueling_dqn = enable_dueling_dqn
 
@@ -32,25 +34,26 @@ class QFunc(tf.keras.Model):
 
         if enable_dueling_dqn:
             self.l4 = Dense(1, name="L3", activation="linear")
-
+        
         self(inputs=tf.constant(np.zeros(shape=(1,)+state_shape, dtype=np.float32)))
 
     def call(self, inputs):
         features = self.l1(inputs)
         features = self.l2(features)
+        q_values_temp = self.l3(features)
         if self._enable_dueling_dqn:
-            advantages = self.l3(features)
+            advantages = q_values_temp
             v_values = self.l4(features)
             q_values = v_values + (advantages - tf.reduce_mean(advantages, axis=1, keepdims=True))
         else:
-            q_values = self.l3(features)
+            q_values = q_values_temp
         return q_values
 
 
 class DQN(tf.keras.Model):
-    def __init__(self, state_shape, action_dim, name="DQN", lr=0.001, units=[32, 32], epsilon=0.1,
+    def __init__(self, state_shape, action_dim, name="DQN", lr=0.001, units=[16, 8], epsilon=0.1,
         epsilon_min=None, epsilon_decay_step=int(1e6), target_replace_interval=int(5e3),
-        enable_double_dqn=False, enable_dueling_dqn=False, discount=None):
+        enable_double_dqn=False, enable_dueling_dqn=False, discount=None, load_model=None):
         super().__init__()
 
         # Define and initialize Q-function network
@@ -58,7 +61,8 @@ class DQN(tf.keras.Model):
             "state_shape": state_shape,
             "action_dim": action_dim,
             "units": units,
-            "enable_dueling_dqn": enable_dueling_dqn}
+            "enable_dueling_dqn": enable_dueling_dqn,
+            "load_model": load_model}
         self.q_func = QFunc(**kwargs_dqn)
         self.q_func_target = QFunc(**kwargs_dqn)
         self.q_func_optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
@@ -90,6 +94,7 @@ class DQN(tf.keras.Model):
         self.max_grad = 10
 
     def get_action(self, state, test=False, tensor=False):
+
         if not tensor:
             assert isinstance(state, np.ndarray)
         is_single_input = state.ndim == self._state_ndim
@@ -105,8 +110,7 @@ class DQN(tf.keras.Model):
             else:
                 return action
 
-        state = np.expand_dims(state, axis=0).astype(
-            np.float32) if is_single_input else state
+        state = np.expand_dims(state, axis=0).astype(np.float32) if is_single_input else state
         action = self._get_action_body(tf.constant(state))
         if tensor:
             return action
@@ -160,27 +164,22 @@ class DQN(tf.keras.Model):
         batch_size = states.shape[0]
         not_dones = 1. - tf.cast(dones, dtype=tf.float32)
         actions = tf.cast(actions, dtype=tf.int32)
-        indices = tf.concat(
-            values=[tf.expand_dims(tf.range(batch_size), axis=1),
-                    actions], axis=1)
-        current_Q = tf.expand_dims(
-            tf.gather_nd(self.q_func(states), indices), axis=1)
+        indices = tf.concat(values=[tf.expand_dims(tf.range(batch_size), axis=1), actions], axis=1)
+        current_Q = tf.expand_dims(tf.gather_nd(self.q_func(states), indices), axis=1)
 
         if self._enable_double_dqn:
-            max_q_indexes = tf.argmax(self.q_func(next_states),
-                                      axis=1, output_type=tf.int32)
-            indices = tf.concat(
-                values=[tf.expand_dims(tf.range(batch_size), axis=1),
-                        tf.expand_dims(max_q_indexes, axis=1)], axis=1)
-            target_Q = tf.expand_dims(
-                tf.gather_nd(self.q_func_target(next_states), indices), axis=1)
+            max_q_indexes = tf.argmax(self.q_func(next_states),axis=1, output_type=tf.int32)
+            indices = tf.concat(values=[tf.expand_dims(tf.range(batch_size), axis=1),tf.expand_dims(max_q_indexes, axis=1)], axis=1)
+            target_Q = tf.expand_dims(tf.gather_nd(self.q_func_target(next_states), indices), axis=1)
             target_Q = rewards + not_dones * self.discount * target_Q
         else:
-            target_Q = rewards + not_dones * self.discount * tf.reduce_max(
-                self.q_func_target(next_states), keepdims=True, axis=1)
+            target_Q = rewards + not_dones * self.discount * tf.reduce_max(self.q_func_target(next_states), keepdims=True, axis=1)
         target_Q = tf.stop_gradient(target_Q)
         td_errors = current_Q - target_Q
         return td_errors
+
+    def save_model(self):
+        print("I wish I could save")
 
 
 class Actor(tf.keras.Model):
@@ -191,15 +190,12 @@ class Actor(tf.keras.Model):
         self.l2 = Dense(units[1], name="L2")
         self.l3 = Dense(action_dim, name="L3")
 
-        self.max_action = max_action
+        # self(tf.constant(np.zeros(shape=(1,)+state_shape, dtype=np.float32)))
 
-        self(tf.constant(np.zeros(shape=(1,)+state_shape, dtype=np.float32)))
-
-    def call(self, inputs):
+    def call(self, inputs, max_action):
         features = tf.nn.relu(self.l1(inputs))
         features = tf.nn.relu(self.l2(features))
-        features = self.l3(features)
-        action = self.max_action * tf.nn.tanh(features)
+        action = tf.nn.tanh(self.l3(features)) * max_action
         return action
 
 
@@ -211,9 +207,9 @@ class Critic(tf.keras.Model):
         self.l2 = Dense(units[1], name="L2")
         self.l3 = Dense(1, name="L3")
 
-        dummy_state = tf.constant(np.zeros(shape=(1,)+state_shape, dtype=np.float32))
-        dummy_action = tf.constant(np.zeros(shape=[1, action_dim], dtype=np.float32))
-        self([dummy_state, dummy_action])
+        # dummy_state = tf.constant(np.zeros(shape=(1,)+state_shape, dtype=np.float32))
+        # dummy_action = tf.constant(np.zeros(shape=[1, action_dim], dtype=np.float32))
+        # self([dummy_state, dummy_action])
 
     def call(self, inputs):
         states, actions = inputs
@@ -226,12 +222,12 @@ class Critic(tf.keras.Model):
 
 class DDPG(tf.keras.Model):
     def __init__(self, state_shape, action_dim, name="DDPG", max_action=1., lr_actor=0.001, lr_critic=0.001, actor_units=[400, 300],
-        critic_units=[400, 300], sigma=0.1, tau=0.005, discount=None, **kwargs):
-        super().__init__(**kwargs)
+        critic_units=[400, 300], sigma=0.1, tau=0.005, discount=None, load_model=None):
+        super().__init__()
 
         # Define and initialize Actor network
-        self.actor = Actor(state_shape, action_dim, max_action, actor_units)
-        self.actor_target = Actor(state_shape, action_dim, max_action, actor_units)
+        self.actor = Actor(state_shape, action_dim, actor_units)
+        self.actor_target = Actor(state_shape, action_dim, actor_units)
         self.actor_optimizer = tf.keras.optimizers.Adam(learning_rate=lr_actor)
         update_target_variables(self.actor_target.weights,self.actor.weights, tau=1.)
 
@@ -248,7 +244,7 @@ class DDPG(tf.keras.Model):
         # Oops
         self.discount=discount
         self.max_grad = 10
-        # self.device = "/gpu:0"#"/cpu:0"
+        self.max_action = max_action
 
     def get_action(self, state, test=False, tensor=False):
         is_single_state = len(state.shape) == 1
@@ -258,7 +254,7 @@ class DDPG(tf.keras.Model):
             np.float32) if is_single_state else state
         action = self._get_action_body(
             tf.constant(state), self.sigma * (1. - test),
-            tf.constant(self.actor.max_action, dtype=tf.float32))
+            tf.constant(self.max_action, dtype=tf.float32))
         if tensor:
             return action
         else:
@@ -266,8 +262,7 @@ class DDPG(tf.keras.Model):
 
     @tf.function
     def _get_action_body(self, state, sigma, max_action):
-        # with tf.device(self.device):
-        action = self.actor(state)
+        action = self.actor(state, max_action)
         action += tf.random.normal(shape=action.shape, mean=0., stddev=sigma, dtype=tf.float32)
         return tf.clip_by_value(action, -max_action, max_action)
 
@@ -279,22 +274,18 @@ class DDPG(tf.keras.Model):
 
     @tf.function
     def _train_body(self, states, actions, next_states, rewards, done, weights):
-        # with tf.device(self.device):
         with tf.GradientTape() as tape:
-            td_errors = self._compute_td_error_body(
-                states, actions, next_states, rewards, done)
+            td_errors = self._compute_td_error_body(states, actions, next_states, rewards, done)
             critic_loss = tf.reduce_mean(huber_loss(td_errors, delta=self.max_grad) * weights)
 
-        critic_grad = tape.gradient(
-            critic_loss, self.critic.trainable_variables)
+        critic_grad = tape.gradient(critic_loss, self.critic.trainable_variables)
         self.critic_optimizer.apply_gradients(zip(critic_grad, self.critic.trainable_variables))
 
         with tf.GradientTape() as tape:
-            next_action = self.actor(states)
+            next_action = self.actor(states, self.max_action)
             actor_loss = -tf.reduce_mean(self.critic([states, next_action]))
 
-        actor_grad = tape.gradient(
-            actor_loss, self.actor.trainable_variables)
+        actor_grad = tape.gradient(actor_loss, self.actor.trainable_variables)
         self.actor_optimizer.apply_gradients(zip(actor_grad, self.actor.trainable_variables))
 
         # Update target networks
@@ -313,12 +304,13 @@ class DDPG(tf.keras.Model):
 
     @tf.function
     def _compute_td_error_body(self, states, actions, next_states, rewards, dones):
-        # with tf.device(self.device):
         not_dones = 1. - dones
-        target_Q = self.critic_target(
-            [next_states, self.actor_target(next_states)])
+        target_Q = self.critic_target([next_states, self.actor_target(next_states, self.max_action)])
         target_Q = rewards + (not_dones * self.discount * target_Q)
         target_Q = tf.stop_gradient(target_Q)
         current_Q = self.critic([states, actions])
         td_errors = target_Q - current_Q
         return td_errors
+
+    def save_model(self):
+        print("I wish I could save")
--- Neural network - car game/tf2rl-reworked/run_algo.py
+++ Neural network - car game/tf2rl-reworked/run_algo.py
@@ -1,37 +1,101 @@
-import gym
+# Import libraries
+import os
+
+import numpy as np
+import tensorflow as tf
+from gym.spaces.box import Box
+from gym.spaces.discrete import Discrete
+from cpprb import ReplayBuffer, PrioritizedReplayBuffer
 
 from algos import DQN, DDPG
-from trainer import Trainer
-
 from homegym import CarGameEnv
 
-continuous = True
+# Define user functions
+def evaluate_policy():
+    episode_return = 0.
+    obs = test_env.reset()
+    done = False
+    while not done:
+        action = agent.get_action(obs, test=True)
+        next_obs, reward, done, _ = test_env.step(action)
+        if show_test_progress:
+            test_env.render()
+        episode_return += reward
+        obs = next_obs
+    return episode_return
 
+# Define variables
+continuous = True
+use_prioritized_rb=False
+show_test_progress=True
+max_steps=1_000_000
+test_interval=1_000
+memory_capacity=1_000_000
+batch_size=64
+
+# Initialise environment, policy and replay buffer
 env = CarGameEnv(continuous)
 test_env = CarGameEnv(continuous)
-
 if continuous:
-    policy = DDPG(
+    agent = DDPG(
         state_shape=env.observation_space.shape,
         action_dim=env.action_space.high.size,
         discount=0.99,
-        max_action=env.action_space.high[0])
+        max_action=env.action_space.high[0]
+        )
 else:
-    policy = DQN(
+    agent = DQN(
         state_shape=env.observation_space.shape,
         action_dim=env.action_space.n,
         discount=0.99,
-        enable_double_dqn=False,
-        enable_dueling_dqn=False,
-        target_replace_interval=300)
+        enable_double_dqn=True,
+        enable_dueling_dqn=True,
+        target_replace_interval=300
+        )
+obs_space_shape = env.observation_space.shape
+act_space_shape = env.action_space.shape if continuous else [1, ]
+env_dict={
+"obs": {"shape": obs_space_shape},
+"next_obs": {"shape": obs_space_shape},
+"act": {"shape": act_space_shape},
+"rew": {},
+"done": {}
+}
+if use_prioritized_rb:
+    replay_buffer = PrioritizedReplayBuffer(size=memory_capacity, default_dtype=np.float32, env_dict=env_dict)
+else:
+    replay_buffer = ReplayBuffer(size=memory_capacity, default_dtype=np.float32, env_dict=env_dict)
 
-trainer = Trainer(
-    policy=policy,
-    env=env,
-    use_prioritized_rb=False,
-    show_test_progress=True,
-    max_steps=1e6,
-    test_interval=1e3,
-    memory_capacity=1e6,
-    batch_size=64)
-trainer()
+# Train agent
+best_score = None
+obs = env.reset()
+for total_steps in range(max_steps):
+
+    # take and store action
+    action = agent.get_action(obs)
+    next_obs, reward, done, _ = env.step(action)
+    replay_buffer.add(obs=obs, act=action, next_obs=next_obs, rew=reward, done=done)
+    obs = next_obs
+    if done:
+        obs = env.reset()
+
+    # extract samples from buffer and train
+    samples = replay_buffer.sample(batch_size)
+    agent.train(
+        samples["obs"], samples["act"], samples["next_obs"],
+        samples["rew"], np.array(samples["done"], dtype=np.float32),
+        None if not use_prioritized_rb else samples["weights"])
+    if use_prioritized_rb:
+        td_error = agent.compute_td_error(
+            samples["obs"], samples["act"], samples["next_obs"],
+            samples["rew"], np.array(samples["done"], dtype=np.float32))
+        replay_buffer.update_priorities(samples["indexes"], np.abs(td_error) + 1e-6)
+
+    # evaluate performance
+    if total_steps == 0 or total_steps % test_interval == 0:
+        avg_test_return = evaluate_policy()
+        save_flag=False
+        print("Evaluation Total Steps: {0: 7} Average Reward {1: 5.4f}".format(total_steps, avg_test_return))
+        if best_score is None or avg_test_return>best_score:
+            agent.save_model()
+            best_score=avg_test_return
